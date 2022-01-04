771d242bf465656eda301d634de4db55cecea225
hilmanzfr <hilman017@gmail.com>
2022-01-04 21:14:29 +0800

    fix margin in about page

--- components/About.js
+++ components/About.js
@@ -12,21 +12,21 @@ function About() {
         </div> */}
         <div className="flex flex-col md:ml-10">
           <h1 className="text-4xl sm:text-5xl text-slate-300 mb-7 text-center lg:text-left">👋🏼</h1>
-          <p className="text-xl sm:text-2xl text-slate-300 mb-7 leading-9 text-center sm:text-left">
+          <p className="text-xl sm:text-2xl text-slate-300 mb-7 leading-9 text-center md:text-left">
             I&apos;ve spent the past 3+ years working and trying out different areas of technologies; front-end development, mobile-app development, UI/UX, artificial intelligence, and game development.</p>
-          <p className="text-xl sm:text-2xl text-slate-300 mb-7 leading-9 text-center sm:text-left">I&apos;m into any sort of software development that requires visualization and creativity. My other areas of interest include machine/deep learning, front-end development and UI/UX.</p>
-          <p className="text-xl sm:text-2xl text-slate-300 mb-7 leading-9 text-center sm:text-left">Beside tech-related stuffs, I spend my time building fun projects, listening to podcasts, playing guitar, and sometimes draw!</p>
+          <p className="text-xl sm:text-2xl text-slate-300 mb-7 leading-9 text-center md:text-left">I&apos;m into any sort of software development that requires visualization and creativity. My other areas of interest include machine/deep learning, front-end development and UI/UX.</p>
+          <p className="text-xl sm:text-2xl text-slate-300 mb-7 leading-9 text-center md:text-left">Beside tech-related stuffs, I spend my time building fun projects, listening to podcasts, playing guitar, and sometimes draw!</p>
         </div>
       </div>
 
       <div className="flex flex-col space-y-14 md:flex-row md:space-x-14 md:space-y-0">
-        <div className="flex flex-col w-2/5">
+        <div className="flex flex-col md:w-2/5">
           <h1 className="text-4xl sm:text-5xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#50d89f]  to-[#6a7eda] mb-7">Work.</h1>
           <p className="text-xl sm:text-2xl text-slate-300 mb-4">MACD Engineered<span className="text-xl sm:text-2xl text-slate-500 pl-3">09/21-11/21</span></p>
           <p className="text-xl sm:text-2xl text-slate-300 mb-4">Robert Bosch<span className="text-xl sm:text-2xl text-slate-500 pl-3">09/20-04/21</span></p>
           <p className="text-xl sm:text-2xl text-slate-300 mb-4">Matte Brands<span className="text-xl sm:text-2xl text-slate-500 pl-3">04/21-06/22</span></p>
         </div>    
-        <div className="flex flex-col w-3/5 md:pl-5">
+        <div className="flex flex-col md:w-3/5 md:pl-5">
           <h1 className="text-4xl sm:text-5xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#50d89f]  to-[#6a7eda] mb-7">Studies.</h1>
           <p className="text-xl sm:text-2xl font-medium text-slate-300 mb-4">Universiti Teknologi PETRONAS</p>
           <p className="text-xl sm:text-2xl text-slate-300 mb-4">Bachelor of Information Technology (Hons)</p>
